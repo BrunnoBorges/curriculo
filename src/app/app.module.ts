a8e20ae52ffb8d55ea336c93af19b0e103f5fd8c
Brunno Diego Oliveira Borges <brunno.borges@BOMPMOM010096.local>
2021-02-23 22:30:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProfessionalComponent } from './professional/professional.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CoursesComponent } from './courses/courses.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProfessionalComponent,
+    ExperienceComponent,
+    CoursesComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/professional/professional.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professional',
+  templateUrl: './professional.component.html',
+  styleUrls: ['./professional.component.scss']
+})
+export class ProfessionalComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(){
+
+  }
+
+}
