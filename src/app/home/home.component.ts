38fb602db714711803dd31092facd36f53a0b24b
brunno.borges <brunno.borges@institutojef.org.br>
2023-11-30 22:34:04 -0300

    realizando algusn ajustes no layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,29 +4,35 @@ import { CurriculoService } from '../services/curriculo.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   namePerson: string;
-  abreviationPerson:string;
+  sobreNomePerson: string;
+  abreviationPerson: string;
   descriptionPerson: string;
   professionPerson: string;
   professionSecundaryPerson: string;
 
-
-  constructor(private curriculoService: CurriculoService) { }
+  constructor(private curriculoService: CurriculoService) {}
 
   ngOnInit(): void {
-    this.curriculoService.getHomeDetails().subscribe((data) => {
-      this.namePerson = data[0].name.split(' ').slice(0, 2).join(' ');;
-      this.abreviationPerson = data[0].name.slice(0, 1);
-      this.descriptionPerson = data[0].description;
-      this.professionPerson = data[0].profession;
-      this.professionSecundaryPerson = data[0].professionSecundary;
-    }, (error) => {
-      console.log('error', error)
-    });
-  }
+    this.curriculoService.getHomeDetails().subscribe(
+      (data) => {
+        console.log('data', data[0]);
+        const nomeCompleto = data[0]?.name;
+        const partesDoNome = nomeCompleto.split(' ');
 
+        this.namePerson = partesDoNome[0];
+        this.sobreNomePerson = partesDoNome[partesDoNome.length - 1];
+        this.abreviationPerson = data[0].name.slice(0, 1);
+        this.descriptionPerson = data[0].description;
+        this.professionPerson = data[0].profession;
+        this.professionSecundaryPerson = data[0].professionSecundary;
+      },
+      (error) => {
+        console.log('error', error);
+      }
+    );
+  }
 }
